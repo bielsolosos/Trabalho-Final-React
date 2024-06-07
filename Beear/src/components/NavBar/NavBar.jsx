2e05d652d9eae9860204049baa96fc7335e980a5
Edd-all <eduardoalmeida2970@gmail.com>
2024-06-07 17:16:39 -0300

    comitando mudanças na NavBar

--- Beear/src/components/NavBar/NavBar.jsx
+++ Beear/src/components/NavBar/NavBar.jsx
@@ -1,15 +1,36 @@
 import React from "react";
 import beearBeerImg from "./BeearBeer.png";
 // import NavbarStyle from "NavbarStyle";
+import {useNavigate} from "react-router-dom";
+
 
 function Navbar() {
+
+    let navegar = useNavigate();
+
+    const mudarRotaHome = () =>{
+        let rota = 'novaRota'; // colocar a rota da home
+        navegar(rota);
+    }
+
+    const mudarRotaCarrinho = () =>{
+        let rota = 'novaRota'; // colocar a rota do carrinho
+        navegar(rota);
+    }
+
+    const mudarRotaSobre = () =>{
+        let rota = 'novaRota'; // colocar a rota do sobre
+        navegar(rota);
+    }
+
+
     return(
         <div className="navbar">
                 <div className="navbar-img"><a href="#"><img src={beearBeerImg} alt="Imagem" /></a></div>
                 <ul className="navbar-menu">
-                    <li>home</li>
-                    <li>carrinho</li>
-                    <li>sobre</li>
+                    <Botao title={'cópia não comédia'} onclick={mudarRotaHome} valor={Navbar}>home</Botao>
+                    <Botao title={'cópia não comédia'} onclick={mudarRotaCarrinho} valor={Navbar}>carrinho</Botao>
+                    <Botao title={'cópia não comédia'} onclick={mudarRotaSobre} valor={Navbar}>sobre</Botao>
                 </ul>
         </div>
     )
