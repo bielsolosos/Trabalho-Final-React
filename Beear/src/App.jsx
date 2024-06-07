426b6850b23c0aa6ed6e1552cde3a983686e6312
Mateus Daltro <162245009+RandomlyActing@users.noreply.github.com>
2024-06-07 13:54:05 -0300

    Adicionando a div que envolve a tela toda

--- Beear/src/App.jsx
+++ Beear/src/App.jsx
@@ -3,14 +3,16 @@ import { CaixaTexto } from './components/CaixaDeTextos/CaixaTexto'
 import { Botao } from './components/Botao/Botao'
 import './App.css'
 import { CarrinhoDeCompras } from './pages/CarrinhoDeCompras/CarrinhoDeCompras'
+import Navbar from './components/NavBar/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='divFullScrean'>
+    <Navbar/>
     <CarrinhoDeCompras/>
-    </>
+    </div>
   )
 }
 
