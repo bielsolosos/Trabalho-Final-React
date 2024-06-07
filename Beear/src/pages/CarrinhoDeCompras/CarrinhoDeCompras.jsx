910159d1081853b79027f61e768f00424a23c496
Edd-all <eduardoalmeida2970@gmail.com>
2024-06-06 21:31:44 -0300

    comitando

--- Beear/src/pages/CarrinhoDeCompras/CarrinhoDeCompras.jsx
+++ Beear/src/pages/CarrinhoDeCompras/CarrinhoDeCompras.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react"
 {/* Lembrar de importar de onde virá a api de cervejas como no modelo abaixo*/} 
 {/* import {getAllCervas} from  '../../services/api'*/} 
 
-export const ListaDeCervejas = ()=> {
+export const CarrinhoDeCompras = ()=> {
 
     const [cervejas, setCervas] = useState([]);
 
@@ -21,12 +21,26 @@ export const ListaDeCervejas = ()=> {
 
     return(
         <>
-            <h1>Lista de Cervejas da Bear Beer!</h1>
+        <h1>Lista de Cervejas da Bear Beer!</h1>
          <ul>
             {cervejas.map(cerva=>(
                 <li key={cerva.index}>{cerva.name}</li>
             ))}
+
+            <li>
+                <p>{cervejas.name}</p>
+
+                <Botao title={"botão para adicionar produto"} 
+                valor={"clique aqui"} 
+                onClick={contar}>Adicionar ao carrinho</Botao>
+
+                <Botao title={"botão para remover produto"} 
+                valor={"clique aqui"} 
+                onClick={contar}>Remover do carrinho</Botao>
+            </li>
          </ul>
+
+
         </>
     )
 }
