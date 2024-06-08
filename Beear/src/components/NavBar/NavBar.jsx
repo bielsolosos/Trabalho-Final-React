3dfbb9dbc08da5c71dec9448dd6a1e842888929c
bielsolosos <bielrochasantoscoutinho@gmail.com>
2024-06-07 22:28:50 -0300

    Algumas alterações na NavBar e nas Rotas

--- Beear/src/components/NavBar/NavBar.jsx
+++ Beear/src/components/NavBar/NavBar.jsx
@@ -8,28 +8,34 @@ function Navbar() {
     let navegar = useNavigate(); // lembre de dar o comando "npm install react-router-dom" para não dar erro
 
     const mudarRotaHome = () =>{
-        let rota = 'novaRota'; // colocar a rota da home
+        let rota = '/'; // colocar a rota da home
         navegar(rota);
     }
 
     const mudarRotaCarrinho = () =>{
-        let rota = 'novaRota'; // colocar a rota do carrinho
+        let rota = '/carrinhodecompras'; // colocar a rota do carrinho
         navegar(rota);
     }
 
     const mudarRotaSobre = () =>{
-        let rota = 'novaRota'; // colocar a rota do sobre
+        let rota = '/'; // colocar a rota do sobre
         navegar(rota);
     }
 
+    const mudarRotaLogin = () =>{
+        let rota = '/login'; // colocar a rota do sobre
+        navegar(rota);
+    }
 
     return(
         <div className={styles.navbar}>
                 <div className={styles.navbarImg}><a href="#"><img src={beearBeerImg} alt="Imagem" /></a></div>
                 <div className={styles.navbarMenu}>
-                    <Botao title={'cópia não comédia'} onclick={mudarRotaHome} valor={'home'}>home</Botao>
-                    <Botao title={'cópia não comédia'} onclick={mudarRotaCarrinho} valor={'carrinho'}>carrinho</Botao>
-                    <Botao title={'cópia não comédia'} onclick={mudarRotaSobre} valor={'sobre'}>sobre</Botao>
+
+                    <Botao title={'cópia não comédia'} onclick={mudarRotaHome} valor={'home'}/>
+                    <Botao title={'cópia não comédia'} onclick={mudarRotaSobre} valor={'sobre'}/>
+                    <Botao title={'cópia não comédia'} onclick={mudarRotaCarrinho} valor={'carrinho'}/>
+                    <Botao title={'cópia não comédia'} onclick={mudarRotaLogin} valor={'login'}/>
                 </div>
         </div>
     )
