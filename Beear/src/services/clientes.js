8444f6114925b69d1b01940f44380cad2410c1bb
bielsolosos <bielrochasantoscoutinho@gmail.com>
2024-06-10 20:10:15 -0300

    COMMITS Q SE DEUS QUISER NN VAI DAR MERGE

--- Beear/src/services/clientes.js
+++ Beear/src/services/clientes.js
@@ -5,3 +5,14 @@ export const getALLClientes= () => {
     const url = '/clientes/listarClientes';
     return api.get(url);
 }
+
+export const criarCliente = async (cliente, cep) => {
+    try {
+        const url = `/clientes/cadastrar/${cep}`
+      const response = api.post(url);
+      return response.data;
+    } catch (error) {
+      console.error('Erro ao criar cliente:', error);
+      throw error;
+    }
+  };
