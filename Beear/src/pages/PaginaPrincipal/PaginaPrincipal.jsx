40c56b4b5385f0d2c44efb07a88273a442100203
AleCurzi <alexandreccurzi@gmail.com>
2024-06-10 21:34:14 -0300

    Home page

--- Beear/src/pages/PaginaPrincipal/PaginaPrincipal.jsx
+++ Beear/src/pages/PaginaPrincipal/PaginaPrincipal.jsx
@@ -1,15 +1,13 @@
+import  {Banner} from "../../components/Banner/Banner"
 import Navbar from "../../components/NavBar/NavBar"
-import {Fundo} from './paginaPstyle.js';
 
 export function PaginaPrincipal(){
 
         return(
-            <>
-                <Fundo> 
-                <Navbar/>
-                <h1>Bem Vindo a Beear Beer teste</h1>
-                </Fundo>
-            </>
-        )
 
-};
+            <div>
+                <Navbar/>
+                <Banner/>
+            </div>
+        )
+}
